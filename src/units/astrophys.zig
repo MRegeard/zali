7f1738e19cb043083b7c5664e6b37d4dabfd277b
Maxime Regeard <regeard@apc.in2p3.fr>
2026-03-03 23:42:50 +0100

    fix unit definition

--- src/units/astrophys.zig
+++ src/units/astrophys.zig
@@ -4,10 +4,15 @@ const utils = @import("utils.zig");
 const unit = @import("unit.zig");
 const si = @import("si.zig");
 const Unit = unit.Unit;
+const Quantity = @import("quantity.zig").Quantity;
 const defUnitFromUnit = utils.defUnitFromUnit;
 const defUnitFromConst = utils.defUnitFromConst;
 
 const PI_F64: f64 = std.math.pi;
+const yr_quantity: Quantity(f64, si.yr) = .init(1.0);
+const s_quantity: Quantity(f64, si.s) = .init(1.0);
+const lyr_quantity = cst.c.quantity.mul(yr_quantity).to(si.m) catch unreachable;
+const lsec_quantity = cst.c.quantity.mul(s_quantity).to(si.m) catch unreachable;
 
 // Length
 //
@@ -17,8 +22,8 @@ pub const kpc: Unit = defUnitFromConst(cst.kpc, .initUniqueSymbol("kpc"));
 pub const solRad: Unit = defUnitFromConst(cst.R_sun, .initUniqueSymbol("solRad"));
 pub const jupiterRad: Unit = defUnitFromConst(cst.R_jup, .initUniqueSymbol("jupiterRad"));
 pub const earthRad: Unit = defUnitFromConst(cst.R_earth, .initUniqueSymbol("earthRad"));
-pub const lyr: Unit = defUnitFromUnit(cst.c.quantity.mul(si.yr).to(si.m), 1.0, .initUniqueSymbol("lyr"));
-pub const lsec: Unit = defUnitFromUnit(cst.c.mul(si.s).to(si.m), 1.0, .initUniqueSymbol("lsec"));
+pub const lyr: Unit = defUnitFromUnit(si.m, lyr_quantity.value, .initUniqueSymbol("lyr"));
+pub const lsec: Unit = defUnitFromUnit(si.m, lsec_quantity.value, .initUniqueSymbol("lsec"));
 
 // Mass
 //
