cc2725e3b4f79b2685b2adc93259ed92aa5f7057
Maxime Regeard <regeard@apc.in2p3.fr>
2026-05-05 18:12:54 +0200

    move utils function from quantity and equivalency to utils.zig

--- src/units/utils.zig
+++ src/units/utils.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
+
 const constants = @import("../constants.zig");
 const Constant = constants.Constant;
 const Unit = @import("unit.zig").Unit;
 const SymbolExpression = @import("symbol.zig").SymbolExpression;
+const Dim = @import("dim.zig").Dim;
+const si = @import("si.zig");
 
 pub fn defUnitFromUnit(
     comptime root: Unit,
@@ -32,4 +35,28 @@ pub fn compErrOrPanic(comptime msg: []const u8) void {
     } else {
         @panic(msg);
     }
+}
+
+pub fn isSpectralUnit(comptime unit: Unit) bool {
+    const wavelength_dim: Dim = si.m.dim;
+    const energy_dim: Dim = si.J.dim;
+    const frequency_dim: Dim = si.Hz.dim;
+    const wavenumber_spec_dim: Dim = si.m.pow(-1).dim;
+    const wavenumber_ang_dim: Dim = si.rad.div(si.m).dim;
+    return comptime unit.dim.eql(wavelength_dim) or
+        unit.dim.eql(energy_dim) or
+        unit.dim.eql(frequency_dim) or
+        unit.dim.eql(wavenumber_spec_dim) or
+        unit.dim.eql(wavenumber_ang_dim);
+}
+
+pub inline fn isQuantity(comptime T: type) bool {
+    return (@hasDecl(T, "is_zali_quantity") and T.is_zali_quantity == true);
+}
+
+pub inline fn assertIsQuantity(comptime T: type) void {
+    if (!isQuantity(T)) {
+        @compileError("Expected a Quantity type, got " ++ @typeName(T));
+    }
+    return;
 }
